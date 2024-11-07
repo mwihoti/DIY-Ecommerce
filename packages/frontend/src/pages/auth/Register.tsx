2e808b6cd439656a4ba8240bbf953b9000f6eb20
mwihoti <danielmwihoti@gmail.com>
2024-11-07 22:00:55 +0300

    Register new users

--- packages/frontend/src/pages/auth/Register.tsx
+++ packages/frontend/src/pages/auth/Register.tsx
@@ -6,9 +6,9 @@ import { RegisterRequest } from '../../services/auth/types';
 
 const Register = ({
     isAuthenticated,
-}: { isAuthenticated: boolean}) => {
+}: { isAuthenticated: boolean }) => {
     const navigate = useNavigate();
-    const [register, {isLoading }] = useRegisterMutation();
+    const [register, { isLoading }] = useRegisterMutation();
     const [registerFormData, setRegisterFormData] = useState<RegisterRequest>({
         username: "",
         email: "",
@@ -17,27 +17,83 @@ const Register = ({
 
     return (
         <div className="card">
-            { !isAuthenticated && (
+            {!isAuthenticated && (
                 <>
-                <h2>Register to our blogging platform</h2>
-                <form 
-                    className="login"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        try {
-                            register(registerFormData)
-                                .then((data) => {
-                                    if (data?.data?.ok) {
-                                        return navigate("/", {
-                                            replace: true
-                                        })
-                                    }
-                                })
+                    <h2>Register to our blogging platform</h2>
+                    <form
+                        className="login"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            try {
+                                register(registerFormData)
+                                    .then((data) => {
+                                        if (data?.data?.ok) {
+                                            return navigate("/", {
+                                                replace: true
+                                            });
+                                        }
+                                        alert("Invalid credentials");
+                                    })
+                                    .catch(() =>
+                                        alert("Server error! Please file a bug report!"),
+
+                                    );
+                            } catch (err) {
+                                alert(`Failed to register; got ${err} `);
+                            }
+                        }}
+                    />
+                    <input
+                        id="username"
+                        placeholder="Username"
+                        type="text"
+
+                        value={registerFormData.username}
+                        onChange={(e) =>
+                            setRegisterFormData({
+                                ...registerFormData,
+                                username: e.target.value,
+                            })
                         }
-                    }}
-                
-                </>
-            )}
-        </div>
+                    />
+
+                    <input
+                        id="email",
+                    placeholder="email",
+                    type="text",
+                    value={registerFormData.email},
+                    onChange={(e) =>
+                        setRegisterFormData({
+                            ...registerFormData,
+                            email: e.target.value,
+                        })
+                    }
+                    />
+
+                    <input
+                        id="password",
+                    placehodler="password",
+                    value={registerFormData.password},
+                    onchange={(e) =>
+                        setRegisterFormData({
+                            ...registerFormData({
+                                ...registerFormData,
+                                password: e.target.value,
+                            })
+                        })
+                    }
+                    />
+
+                    <button type="submit">
+
+                        {isLoading ? "Registering..." : "Register"}
+                    </button>
+                </form>
+        </>
     )
 }
+        </div >
+    );
+};
+
+export default Register
